fa947b4a11d8edba3b1981e9ebf53dfa84fc983c
Sugata <sarkarsugata57@gmail.com>
2023-12-31 10:58:44 +0530

    Clearing Git cache

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Flair",
+  description: "Expertly designed goods website for your home, travel, and more.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
