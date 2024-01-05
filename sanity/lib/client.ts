e18d5e714797ece41ec5e7b5bcff09df7fa8cef2
Sugata <sarkarsugata57@gmail.com>
2024-01-05 18:36:56 +0530

    token commented

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,5 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:"sk8NE312Sb82Nn3o3uZlNeBcY7eDpnIdP7d27NMvSWuKG4KvWMYDJs3nGS3OerVJHYpP6EpukSrl7XgHoZKM2dfp6UOQyiVZJxAHumXYeZnGd4ECcEj5IlXlHe6EN3ICBNXGhIGM9y7awHsFH2dWnk8CUke6tF2pHlLHZqeeFSy1ta2dN6ZM",
+  // token:"sk8NE312Sb82Nn3o3uZlNeBcY7eDpnIdP7d27NMvSWuKG4KvWMYDJs3nGS3OerVJHYpP6EpukSrl7XgHoZKM2dfp6UOQyiVZJxAHumXYeZnGd4ECcEj5IlXlHe6EN3ICBNXGhIGM9y7awHsFH2dWnk8CUke6tF2pHlLHZqeeFSy1ta2dN6ZM",
 })
